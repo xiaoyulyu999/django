39c7b53b7dfd340272ba0fa1d06752c89eed687a
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-12-16 22:25:43 +0000

    work work

---
+++ core/core/utils/collections.py
@@ -0,0 +1,13 @@
+def deep_update(base_dict, update_dict):
+    for k, v in update_dict.items():
+        if isinstance(v, dict):
+            base_dict_value = base_dict.get(k)
+
+            if isinstance(base_dict_value, dict):
+                deep_update(base_dict_value, v)
+            else:
+                base_dict[k] = v
+        else:
+            base_dict[k] = v
+
+    return base_dict
