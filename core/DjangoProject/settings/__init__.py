e565ab011a7c39c5496f85e25fa747764403270b
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-12-16 23:17:14 +0000

    work work

--- core/DjangoProject/settings/__init__.py
+++ core/DjangoProject/settings/__init__.py
@@ -1,2 +1,15 @@
 from pathlib import Path
+import os.path
+from split_settings.tools import include, optional
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+
+# Namespacing our own custom environment variables
+
+ENVVAR_SETTINGS_PREFIX = 'CORESETTINGS_'
+
+LOCAL_SETTINGS_PATH = os.getenv(ENVVAR_SETTINGS_PREFIX + 'LOCAL_SETTINGS_PATH')
+
+if not LOCAL_SETTINGS_PATH:
+    LOCAL_SETTINGS_PATH = 'local/settings.dev.py'
