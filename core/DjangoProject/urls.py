6d079eb986ffc081b2f316615f5d026aaea0febb
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-12-19 18:46:44 +0000

    later_1

--- core/DjangoProject/urls.py
+++ core/DjangoProject/urls.py
@@ -1,5 +1,11 @@
+import logging
+
 from django.contrib import admin
 from django.urls import path
+
+logger = logging.getLogger(__name__)
+logger.debug("This is a debug message")
+logger.info("This is a info message")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
